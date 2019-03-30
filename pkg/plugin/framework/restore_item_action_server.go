bd88a54bab3220e8d509a8bf0f5529e9a2b5426a
Scott Seago <sseago@redhat.com>
2019-03-30 19:30:09 -0400

    Adds support for allowing a RestoreItemAction to skip item restore
    This allows a RestoreItemAction plugin to signal to velero that
    the returned item should be skipped rather than restored to the
    cluster.
    
    To support this, a boolean SkipRestore attribute is added to
    RestoreItemActionExecuteOutput. If restore.restoreResource finds
    this set to true, any remaining actions on this item are skipped,
    and restore on this item is skipped. Execution continues with
    the next item of this resource type.
    
    To signal this for a particular item, the RestoreItemAction's
    Execute method should call WithoutRestore() on the
    RestoreItemActionExecuteOutput before returning it.
    
    Signed-off-by: Scott Seago <sseago@redhat.com>

--- pkg/plugin/framework/restore_item_action_server.go
+++ pkg/plugin/framework/restore_item_action_server.go
@@ -118,10 +118,12 @@ func (s *RestoreItemActionGRPCServer) Execute(ctx context.Context, req *proto.Re
 	// If the plugin implementation returned a nil updateItem (meaning no modifications), reset updatedItem to the
 	// original item.
 	var updatedItemJSON []byte
+	skipRestore := false
 	if executeOutput.UpdatedItem == nil {
 		updatedItemJSON = req.Item
 	} else {
 		updatedItemJSON, err = json.Marshal(executeOutput.UpdatedItem.UnstructuredContent())
+		skipRestore = executeOutput.SkipRestore
 		if err != nil {
 			return nil, newGRPCError(errors.WithStack(err))
 		}
@@ -129,7 +131,7 @@ func (s *RestoreItemActionGRPCServer) Execute(ctx context.Context, req *proto.Re
 
 	res := &proto.RestoreItemActionExecuteResponse{
 		Item:        updatedItemJSON,
-		SkipRestore: executeOutput.SkipRestore,
+		SkipRestore: skipRestore,
 	}
 
 	for _, item := range executeOutput.AdditionalItems {
