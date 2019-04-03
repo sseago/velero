7a0f0c99a2ba3712dc605fe0f81f076bd361e47a
Scott Seago <sseago@redhat.com>
2019-04-03 16:49:57 -0400

    Incorporated upstream PR review
    This includes:
    1) a bug fix, dealing with a situation in which executeOutput.UpdatedItem is nil
    2) doc comment update
    3) fixed the problems with proto code generation

--- pkg/plugin/framework/restore_item_action_server.go
+++ pkg/plugin/framework/restore_item_action_server.go
@@ -118,12 +118,10 @@ func (s *RestoreItemActionGRPCServer) Execute(ctx context.Context, req *proto.Re
 	// If the plugin implementation returned a nil updateItem (meaning no modifications), reset updatedItem to the
 	// original item.
 	var updatedItemJSON []byte
-	skipRestore := false
 	if executeOutput.UpdatedItem == nil {
 		updatedItemJSON = req.Item
 	} else {
 		updatedItemJSON, err = json.Marshal(executeOutput.UpdatedItem.UnstructuredContent())
-		skipRestore = executeOutput.SkipRestore
 		if err != nil {
 			return nil, newGRPCError(errors.WithStack(err))
 		}
@@ -131,7 +129,7 @@ func (s *RestoreItemActionGRPCServer) Execute(ctx context.Context, req *proto.Re
 
 	res := &proto.RestoreItemActionExecuteResponse{
 		Item:        updatedItemJSON,
-		SkipRestore: skipRestore,
+		SkipRestore: executeOutput.SkipRestore,
 	}
 
 	for _, item := range executeOutput.AdditionalItems {
